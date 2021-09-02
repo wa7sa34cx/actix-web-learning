2f70b368d9221053d8e2a71048bddd53065aed4a
Alex Black <wa7@sa34.cx>
2021-09-02 11:59:42 +0300

    Blocking threads = bad practice

---
+++ examples/delay.rs
@@ -0,0 +1,26 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use std::time::Duration;
+
+#[get("/duration")]
+async fn duration() -> impl Responder {
+    std::thread::sleep(Duration::from_secs(5)); // <-- Bad practice! Will cause the current worker thread to hang!
+    HttpResponse::Ok().body("Delay for 5 seconds...")
+}
+
+#[get("/hello")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello from Server!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(duration)
+            .service(hello)
+    })
+    .workers(1)
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
